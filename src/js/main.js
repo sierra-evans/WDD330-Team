f89bff0407e26d8b1aee485ecedf82e2371e6a58
Sierra Evans <eva22011@byui.edu>
2025-10-31 17:22:15 -0700

    Register modal complete.

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,24 @@ import { loadHeaderFooter } from "./utils.mjs";
 loadHeaderFooter();
 
 productList(".product-list", "tents");
+
+// Register Modal
+function showSignupModal() {
+    const modal = document.getElementById("signupModal");
+    const closeBtn = document.getElementById("closeModal");
+  
+    if (!modal) return;
+  
+    if (!localStorage.getItem("modalShown")) {
+      modal.style.display = "flex";
+      localStorage.setItem("modalShown", "true");
+    }
+  
+    closeBtn?.addEventListener("click", () => {
+      modal.style.display = "none";
+    });
+  }
+  
+  window.addEventListener("load", showSignupModal);
+  // End of Register Modal
+  
