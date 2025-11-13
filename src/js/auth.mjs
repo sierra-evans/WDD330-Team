6bf8eeea0017766ccc4198c040394e4e78e9d153
ButterMyLoaf23 <bostonwyatt1@gmail.com>
2025-11-12 18:14:15 -0700

    BW team 8 changes

---
+++ src/js/auth.mjs
@@ -0,0 +1,41 @@
+import { alertMessage, getLocalStorage, setLocalStorage } from "./utils.mjs";
+import { loginRequest } from "./externalServices.mjs";
+import jwt_decode from "jwt-decode";
+
+const tokenKey = "so-token";
+
+function isTokenValid(token) {
+    if (token) {
+        const decoded = jwt_decode(token);
+        let currentDate = new Date();
+        if (decoded.exp * 1000 < currentDate.getTime()) {
+            console.log("Token expired.");
+            return false;
+        }else {
+            console.log("Valid token");
+            return true;
+        }
+
+    } else return false;
+}
+
+export async function login(creds, redirect = "/") {
+    try {
+        const token = await loginRequest(creds);
+        setLocalStorage(tokenKey, token);
+        window.location = redirect;
+    } catch (err) {
+        alertMessage(err.message.message);
+    }
+}
+
+export function checkLogin() {
+    const token = getLocalStorage(tokenKey);
+    const valid = isTokenValid(token);
+    if (!valid) {
+        localStorage.removeItem(tokenKey);
+        const location = window.location;
+        console.log(location);
+        window.location = `/login/index.html?redirect=${location.pathname}`;
+    } else return token;
+}
