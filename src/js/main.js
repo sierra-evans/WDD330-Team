c394931f9973d6bd7ef516acb0f4a53aa235a693
Sierra Evans <eva22011@byui.edu>
2025-12-17 13:00:59 -0700

    Added newsletter

--- src/js/main.js
+++ src/js/main.js
@@ -54,7 +54,7 @@ function computeDiscount(listPrice, finalPrice) {
 // Only run on pages that have a product-list
 document.addEventListener("DOMContentLoaded", () => renderTopProducts());
 
-// Newsletter Form Handling
+// Newsletter Form
 document.addEventListener("DOMContentLoaded", () => {
 	const button = document.getElementById("subscribe-btn");
 	const message = document.getElementById("newsletter-message");
